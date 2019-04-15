c4f7c49e79aaeeb8c9c8be94cbe5a73863bc39f2
Ethan Buchman <ethan@coinculture.info>
2019-04-15 03:41:37 -0400

    Bank of Canada doesnt support weekends ...

--- cad-usd.sh
+++ cad-usd.sh
@@ -4,6 +4,8 @@ set -eu
 DATE=$1 # 2017-01-31
 
 echo "---------------------------------------"
+
+# NOTE: it looks like the Bank of Canada API doesn't support weekends!
 CAD_USD=$(curl -s "https://www.bankofcanada.ca/valet/observations/FXUSDCAD/json?start_date=$DATE&end_date=$DATE" |  jq .observations[0].FXUSDCAD.v )
 echo "FROM bankofcanada.ca"
 echo "CAD/USD = $CAD_USD"
